b2a0fc7b997f509be3e145412ff2f8713968cbbb
Garrett <garrettgassensmith@gmail.com>
2020-06-05 13:10:07 -0500

    Deleted Exercise model and added to Workout model, added CRUD operations to API routes

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,49 @@
 const router = require("express").Router();
 const db = require("../models");
 
+// CREATE WORKOUT
+router.post("/api/workouts", ({body}, res) => {
+  db.Workout.create(body)
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// GET ALL WORKOUTS
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find()
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find()
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// ADD EXERCISE
+// https://coursework.vschool.io/mongoose-crud/
+router.put("/api/workouts/:id", ({body, params}, res) => {
+  db.Workout.findByIdAndUpdate(params.id, body, {new: true})
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 
   
   module.exports = router;
--- models/Workout.js
+++ models/Workout.js
@@ -1,20 +1,43 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    unique: true
+    required: true,
+    default: Date.now
   },
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
-    }
-  ]
+      type: {
+        type: String,
+        required: "Enter an exercise type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for the exercise",
+      },
+      duration: {
+        type: Number,
+        required: "Enter a duration",
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
 });
-  
-  const Workout = mongoose.model("Workout", WorkoutSchema);
-  
-  module.exports = Workout;
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
